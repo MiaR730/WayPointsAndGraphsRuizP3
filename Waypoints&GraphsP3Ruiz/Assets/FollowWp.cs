668f88d35b2997e77ec9d4ea6b3b9bdcfccd3bf8
MiaR730 <723420@student.dusd.net>
2025-01-10 11:42:45 -0800

    Added more tanks and more to script, fixing sdcript

--- Waypoints&GraphsP3Ruiz/Assets/FollowWp.cs
+++ Waypoints&GraphsP3Ruiz/Assets/FollowWp.cs
@@ -10,28 +10,38 @@ public class FollowWp : MonoBehaviour
     public float speed = 10.0f;
     public float rotSpeed = 10.0f;
 
+    GameObject tracker;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
+    }
+
+    void Progresstracker()
+    {
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 3)
+            currentWP++;
+
+        if (currentWP >= waypoints.Length)
+            currentWP = 0;
+
+        tracker.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.Translate(0, 0, 0.1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
-            currentWP++;
+        Progresstracker();
 
-        if(currentWP >= waypoints.Length)
-            currentWP = 0;
+        Quaternion lookatWP = Quaternion.LookRotation(tracker[currentWP].transform.position - this.transform.position);
 
-        //this.transform.LookAt(waypoints[currentWP].transform);
-
-        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
-
-        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
+       // this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
         
-        this.transform.Translate(0, 0, speed * Time.deltaTime);
+        //this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
